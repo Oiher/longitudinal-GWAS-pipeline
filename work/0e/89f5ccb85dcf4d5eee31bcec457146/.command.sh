510d3b8e24820681301fcaab087fbed46ac0a2b5
Oiher <oihersa@gmail.com>
2025-07-31 15:27:08 +0100

    Removed work cache and files folders from tracking

--- work/0e/89f5ccb85dcf4d5eee31bcec457146/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-set -x
-cat allchr.mergelist.txt | uniq > tmp_mergefile.txt
-plink2 --memory 12288       --pmerge-list "tmp_mergefile.txt"       --make-bed       --out "allchr_SURV_CPH_p2in"
--- work/c0/7c64ef74f95ba3a9afceca363838c2/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-plink2           --indep-pairwise 50 .2           --maf 0.05           --pfile "allchr_LONG_LMM_p2in"           --out EUR_intermediate.ld
-
-plink2           --keep EUR_intermediate_filtered.tsv           --out EUR_intermediate.pca           --extract EUR_intermediate.ld.prune.in           --pca 10           --threads 4           --memory 12288           --pfile "allchr_LONG_LMM_p2in"
